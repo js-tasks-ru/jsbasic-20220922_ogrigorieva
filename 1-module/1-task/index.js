e7873384bd851df0aa5e043023156a72ac8f060f
Olga Grigorieva <olga.grigorieva@sravni.ru>
2022-10-17 22:05:13 +0300

    добавлено решение 0, 1 и 2 модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
-function factorial(n) {
-  // ваш код...
+function factorial(number) {
+
+  let result = 1;
+
+  while (number--) {
+    result *= number + 1;
+  }
+
+  return result;
 }
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name && name.length >= 4 && (name.indexOf(' ') === -1)) {
+    return true;
+  }
+  return false;
 }
 
 function sayHello() {
@@ -22,3 +25,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
